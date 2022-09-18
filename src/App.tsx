e32ff5b34ab4e113cddafac7cb9cbcd675e5fe39
PavelVoitov <voitov.pavel@yandex.ru>
2022-09-19 01:22:07 +0300

    App commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import {Count} from "./components/Count";
 import './App.css';
+import {ButtonForOddEven} from "./components/ButtonForOddEven";
+import {Selector} from "./components/СolorChange/Selector";
+
+const arrOfNumber = [1, 5, 8, 96, 28, 101, 4];
+
+export type FilterType = 'Odd' | 'Even' | 'All';
 
 function App() {
+
+    // console.log(devArr);
+    const [number, setNumber] = useState(arrOfNumber)
+
+    const [filter, setFilter] = useState<FilterType>("All");
+
+    let filterNumber = number;
+    if (filter === "Odd") {
+        filterNumber = number.filter((el) => !(el%2));
+    } else if (filter === "Even") {
+        filterNumber = number.filter((el) => el%2);
+    } else if (filter === "All") {
+        filterNumber = number.filter((el) => el);
+    }
+     const FilterNumber = (num : FilterType) => {
+         setFilter(num);
+     }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          <h1>Site for exams!</h1>
+        <Count/>
+        <ButtonForOddEven arrOfNumber={filterNumber} callback={FilterNumber}/>
+        <Selector />
     </div>
   );
 }
+
 
 export default App;
